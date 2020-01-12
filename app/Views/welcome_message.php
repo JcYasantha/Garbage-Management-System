4d9387fdae4307a6b594c9297c0ab13ac7442b43
JcYasantha <yasantha619@gmail.com>
2020-01-12 17:49:11 +0530

    Dashboard Interface added

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -2,6 +2,7 @@
 <html>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
     integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <head>
     <title>Sign In</title>
@@ -10,7 +11,6 @@
 </head>
 
 <body>
-
     <style {csp-style-nonce}>
     :root {
         --input-padding-x: 1.5rem;
@@ -113,16 +113,6 @@
         color: #777;
     }
 
-    .btn-google {
-        color: white;
-        background-color: #ea4335;
-    }
-
-    .btn-facebook {
-        color: white;
-        background-color: #3b5998;
-    }
-
     /* Fallback for Edge
 -------------------------------------------------- */
 
@@ -184,19 +174,5 @@
             </div>
         </div>
     </div>
-</body>
 
-</html>
-
-<script>
-< script src = "https://code.jquery.com/jquery-3.4.1.slim.min.js"
-integrity = "sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-crossorigin = "anonymous" >
-</script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
-</script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
-</script>
-</script>
+	<?= $this->include('partials/footer') ?>
