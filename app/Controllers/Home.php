0c8ce338c36fa96a006cc4570fa34c2f06725426
JcYasantha <yasantha619@gmail.com>
2020-01-12 19:18:33 +0530

    interface for dustbin create added

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,7 +4,7 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('dashboard');
+		return view('dustbin/create');
 	}
 
 	//--------------------------------------------------------------------
--- app/Views/partials/navbar.php
+++ app/Views/partials/navbar.php
@@ -3,7 +3,8 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
     integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
+<link href="https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/css/select2.min.css" rel="stylesheet" />
+<script src = "https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
 <head>
     <title>Sign In</title>
 
