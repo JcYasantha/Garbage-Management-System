52bfacc84f688a3d56a7c614a9ee405726f28eb3
JcYasantha <yasantha619@gmail.com>
2020-01-12 15:26:02 +0530

    Login Interface

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -1,142 +1,201 @@
 <!doctype html>
 <html>
-	<head>
-		<title>Welcome to CodeIgniter</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-		<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
-	</head>
-	<body>
+<head>
+    <title>Sign In</title>
 
-		<style {csp-style-nonce}>
-			div.logo {
-				height: 200px;
-				width: 155px;
-				display: inline-block;
-				opacity: 0.12;
-				position: absolute;
-				z-index: 0;
-				top: 2rem;
-				left: 50%;
-				margin-left: -73px;
-			}
-			body {
-				height: 100%;
-				background: #fafafa;
-				font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-				color: #777;
-				font-weight: 300;
-			}
-			h1 {
-				font-weight: lighter;
-				letter-spacing: 0.8rem;
-				font-size: 3rem;
-				margin-top: 145px;
-				margin-bottom: 0;
-				color: #222;
-				position: relative;
-				z-index: 1;
-			}
-			.wrap {
-				max-width: 1024px;
-				margin: 5rem auto;
-				padding: 2rem;
-				background: #fff;
-				text-align: center;
-				border: 1px solid #efefef;
-				border-radius: 0.5rem;
-				position: relative;
-			}
-			.version {
-				margin-top: 0;
-				color: #999;
-			}
-			.guide {
-				margin-top: 3rem;
-				text-align: left;
-			}
-			pre {
-				white-space: normal;
-				margin-top: 1.5rem;
-			}
-			code {
-				background: #fafafa;
-				border: 1px solid #efefef;
-				padding: 0.5rem 1rem;
-				border-radius: 5px;
-				display: block;
-			}
-			p {
-				margin-top: 1.5rem;
-			}
-			.footer {
-				margin-top: 2rem;
-				border-top: 1px solid #efefef;
-				padding: 1em 2em 0 2em;
-				font-size: 85%;
-				color: #999;
-			}
-			a:active,
-			a:link,
-			a:visited {
-				color: #dd4814;
-			}
-		</style>
+    <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
+</head>
 
-		<div class="wrap">
+<body>
 
-			<div class="logo">
-				<svg version="1.0" xmlns="http://www.w3.org/2000/svg"
-					 width="155.000000px" height="200.000000px" viewBox="0 0 155.000000 200.000000"
-					 preserveAspectRatio="xMidYMid meet">
-				<g transform="translate(0.000000,200.000000) scale(0.100000,-0.100000)" fill="#ee2600" stroke="none">
-				<path d="M737 1963 c22 -79 -7 -185 -78 -290 -18 -26 -107 -122 -197 -213
-					  -239 -240 -336 -371 -403 -544 -79 -206 -78 -408 5 -582 64 -134 212 -264 361
-					  -314 l60 -20 -30 22 c-210 152 -229 387 -48 588 25 27 48 50 51 50 4 0 7 -27
-					  7 -61 0 -57 2 -62 37 -95 30 -27 46 -34 78 -34 56 0 99 24 116 65 29 69 16
-					  120 -50 205 -105 134 -117 233 -43 347 l31 48 7 -47 c13 -82 58 -129 250 -258
-					  209 -141 306 -261 328 -405 11 -72 -1 -161 -31 -218 -27 -53 -112 -143 -165
-					  -174 -24 -14 -43 -26 -43 -28 0 -2 24 4 53 14 241 83 427 271 482 486 19 76
-					  19 202 -1 285 -35 152 -146 305 -299 412 l-70 49 -6 -33 c-8 -48 -26 -76 -59
-					  -93 -45 -23 -103 -19 -138 10 -67 57 -78 146 -37 305 30 116 32 206 5 291 -27
-					  89 -104 206 -162 247 -17 13 -18 12 -11 -15z"></path>
-				</g>
-				</svg>
-			</div>
+    <style {csp-style-nonce}>
+    :root {
+        --input-padding-x: 1.5rem;
+        --input-padding-y: .75rem;
+    }
 
-			<h1>Welcome to CodeIgniter</h1>
+    body {
+        background: #007bff;
+        background: linear-gradient(to right, #0062E6, #33AEFF);
+    }
 
-			<p class="version">version <?= CodeIgniter\CodeIgniter::CI_VERSION ?></p>
+    .card-signin {
+        border: 0;
+        border-radius: 1rem;
+        box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
+    }
 
-			<div class="guide">
-				<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+    .card-signin .card-title {
+        margin-bottom: 2rem;
+        font-weight: 300;
+        font-size: 1.5rem;
+    }
 
-				<p>If you would like to edit this page you'll find it located at:</p>
+    .card-signin .card-body {
+        padding: 2rem;
+    }
 
-				<pre>
-				<code>
-					app/Views/welcome_message.php
-				</code>
-				</pre>
+    .form-signin {
+        width: 100%;
+    }
 
-				<p>The corresponding controller for this page is found at:</p>
+    .form-signin .btn {
+        font-size: 80%;
+        border-radius: 5rem;
+        letter-spacing: .1rem;
+        font-weight: bold;
+        padding: 1rem;
+        transition: all 0.2s;
+    }
 
-				<pre>
-				<code>
-					app/Controllers/Home.php
-				</code>
-				</pre>
+    .form-label-group {
+        position: relative;
+        margin-bottom: 1rem;
+    }
 
-				<p>If you are exploring CodeIgniter for the very first time, you
-					should start by reading the
-					<a href="https://codeigniter4.github.io/CodeIgniter4">User Guide</a>.</p>
+    .form-label-group input {
+        height: auto;
+        border-radius: 2rem;
+    }
 
-			</div>
+    .form-label-group>input,
+    .form-label-group>label {
+        padding: var(--input-padding-y) var(--input-padding-x);
+    }
 
-			<div class="footer">
-				Page rendered in {elapsed_time} seconds. Environment: <?= ENVIRONMENT ?>
-			</div>
+    .form-label-group>label {
+        position: absolute;
+        top: 0;
+        left: 0;
+        display: block;
+        width: 100%;
+        margin-bottom: 0;
+        /* Override default `<label>` margin */
+        line-height: 1.5;
+        color: #495057;
+        border: 1px solid transparent;
+        border-radius: .25rem;
+        transition: all .1s ease-in-out;
+    }
 
-		</div>
+    .form-label-group input::-webkit-input-placeholder {
+        color: transparent;
+    }
 
-	</body>
+    .form-label-group input:-ms-input-placeholder {
+        color: transparent;
+    }
+
+    .form-label-group input::-ms-input-placeholder {
+        color: transparent;
+    }
+
+    .form-label-group input::-moz-placeholder {
+        color: transparent;
+    }
+
+    .form-label-group input::placeholder {
+        color: transparent;
+    }
+
+    .form-label-group input:not(:placeholder-shown) {
+        padding-top: calc(var(--input-padding-y) + var(--input-padding-y) * (2 / 3));
+        padding-bottom: calc(var(--input-padding-y) / 3);
+    }
+
+    .form-label-group input:not(:placeholder-shown)~label {
+        padding-top: calc(var(--input-padding-y) / 3);
+        padding-bottom: calc(var(--input-padding-y) / 3);
+        font-size: 12px;
+        color: #777;
+    }
+
+    .btn-google {
+        color: white;
+        background-color: #ea4335;
+    }
+
+    .btn-facebook {
+        color: white;
+        background-color: #3b5998;
+    }
+
+    /* Fallback for Edge
+-------------------------------------------------- */
+
+    @supports (-ms-ime-align: auto) {
+        .form-label-group>label {
+            display: none;
+        }
+
+        .form-label-group input::-ms-input-placeholder {
+            color: #777;
+        }
+    }
+
+    /* Fallback for IE
+-------------------------------------------------- */
+
+    @media all and (-ms-high-contrast: none),
+    (-ms-high-contrast: active) {
+        .form-label-group>label {
+            display: none;
+        }
+
+        .form-label-group input:-ms-input-placeholder {
+            color: #777;
+        }
+    }
+    </style>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                <div class="card card-signin my-5">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Sign In</h5>
+                        <form class="form-signin">
+                            <div class="form-label-group">
+                                <input type="email" id="inputEmail" class="form-control" placeholder="Email address"
+                                    required autofocus>
+                                <label for="inputEmail">Email address</label>
+                            </div>
+
+                            <div class="form-label-group">
+                                <input type="password" id="inputPassword" class="form-control" placeholder="Password"
+                                    required>
+                                <label for="inputPassword">Password</label>
+                            </div>
+
+                            <div class="custom-control custom-checkbox mb-3">
+                                <input type="checkbox" class="custom-control-input" id="customCheck1">
+                                <label class="custom-control-label" for="customCheck1">Remember password</label>
+                            </div>
+                            <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign
+                                in</button>
+                            <hr class="my-4">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
 </html>
+
+<script>
+< script src = "https://code.jquery.com/jquery-3.4.1.slim.min.js"
+integrity = "sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+crossorigin = "anonymous" >
+</script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+</script>
+</script>
